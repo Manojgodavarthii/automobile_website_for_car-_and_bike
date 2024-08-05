b8da1f08079ea59930945aba11d34f49a4753958
Manojgodavarthii <141481090+Manojgodavarthii@users.noreply.github.com>
2024-08-05 14:33:08 +0530

    automobile_website.html

---
+++ code/assets.js
@@ -0,0 +1,26 @@
+export const assets={
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const_menu_list=[
+
+    
+]
